29801d92cebcc81a0870f49e06a1404d32f6ef03
Keenan Johns <keenanjj13@protonmail.com>
2026-02-07 11:10:48 -0700

    feat: fixed highlighting issues and added utils to change, delete, and yank

--- lua/smart-motion/actions/yank.lua
+++ lua/smart-motion/actions/yank.lua
@@ -1,5 +1,4 @@
-local log = require("smart-motion.core.log")
-local resolve_range = require("smart-motion.actions.utils").resolve_range
+local utils = require("smart-motion.actions.utils")
 
 ---@type SmartMotionActionModuleEntry
 local M = {}
@@ -10,21 +9,20 @@ local M = {}
 function M.run(ctx, cfg, motion_state)
 	local target = motion_state.selected_jump_target
 	local bufnr = target.metadata.bufnr
-	local start_row, start_col, end_row, end_col = resolve_range(ctx, motion_state)
+	local start_row, start_col, end_row, end_col = utils.resolve_range(ctx, motion_state)
 
+	local text, regtype
 	if target.type == "lines" and not motion_state.exclude_target then
 		local lines = vim.api.nvim_buf_get_lines(bufnr, start_row, end_row + 1, false)
-		vim.fn.setreg('"', table.concat(lines, "\n"), "l")
+		text = table.concat(lines, "\n")
+		regtype = "l"
 	else
-		local text = vim.api.nvim_buf_get_text(bufnr, start_row, start_col, end_row, end_col, {})
-		vim.fn.setreg('"', table.concat(text, "\n"), "c")
+		local lines = vim.api.nvim_buf_get_text(bufnr, start_row, start_col, end_row, end_col, {})
+		text = table.concat(lines, "\n")
+		regtype = "c"
 	end
 
-	vim.highlight.on_yank({
-		higroup = "IncSearch",
-		timeout = 150,
-		on_visual = false,
-	})
+	utils.set_register(bufnr, start_row, start_col, end_row, end_col, text, regtype, "y")
 end
 
 return M
